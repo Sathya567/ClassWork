d8c6814ec3afb55c7ea7d159400e4c591f89ba23
BootCampBL <satyabhandari43@gmail.com>
2021-10-06 15:12:31 +0530

    [satya] added- method to solve Buzz number program

---
+++ BizzNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassWork
+{
+    class BizzNumber
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 7 == 0)
+                Console.WriteLine("Buzz Number");
+            else
+                Console.WriteLine("Not a Buzz Number");
+        }
+    }
+}
+
+   
