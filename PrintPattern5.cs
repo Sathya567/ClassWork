882a74dc1c20424c3c78e67ce41a213e0ebf0cfc
BootCampBL <satyabhandari43@gmail.com>
2021-10-06 15:15:36 +0530

    [satya] added- method to solve 5 row pattern

---
+++ PrintPattern5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassWork
+{
+    class PrintPattern5
+    {
+        static void Main(String[] args)
+        {
+            int i, j;
+            for (i = 0; i <= 4; i++)
+            {
+                for (j = 0; j <= 7; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
+
+   
